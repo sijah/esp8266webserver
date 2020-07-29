e3ccb09b075d9fc253a338b5458865a823588c48
sijah.ak <sijah.ak@gmail.com>
2020-07-29 19:57:00 +0530

    sta

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "o2";
+const char* pass = "asdfghjkl";
 
 void setup() {
+  Serial.begin(115200);
+  Serial.print("connecting to ..");
+  Serial.println(ssid);
+  WiFi.begin(ssid,pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+  
   // put your setup code here, to run once:
 }
 
